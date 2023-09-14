fb57eafed92496cc62b730e0d3520a1dc5ae3b36
Bachti <abdulmuif.bahtiar@gmail.com>
2023-09-14 23:06:05 +0700

    Updated

--- app/config/config.php
+++ app/config/config.php
@@ -3,7 +3,7 @@
 define('BASEURL', 'http://localhost:8000/public');
 
 // DB
-define('DB_HOST', 'localhost');
+define('DB_HOST', '127.0.0.1');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'db_produksi');
