fd4fa62c1000ec2a4a0a6259cba10a10a62fb055
Bachti <abdulmuif.bahtiar@gmail.com>
2023-09-28 13:12:40 +0700

    update 28 September 2023

--- app/views/landing/profile.php
+++ app/views/landing/profile.php
@@ -7,7 +7,7 @@
                 <path d="M16 3.13C16.8604 3.35031 17.623 3.85071 18.1676 4.55232C18.7122 5.25392 19.0078 6.11683 19.0078 7.005C19.0078 7.89318 18.7122 8.75608 18.1676 9.45769C17.623 10.1593 16.8604 10.6597 16 10.88" stroke="#F5F5F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                 <path d="M9 11C11.2091 11 13 9.20914 13 7C13 4.79086 11.2091 3 9 3C6.79086 3 5 4.79086 5 7C5 9.20914 6.79086 11 9 11Z" stroke="#F5F5F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
             </svg>
-            <h2 class="display-5 fw-normal">User Profile</h2>
+            <h2 class="display-5 fw-normal">Admin PPIC Profile</h2>
         </div>
         <div class="bg-light shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;">
             <div class="col-md-9 p-lg-5 mx-auto">
